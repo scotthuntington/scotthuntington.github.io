b7e6f10a2f9a9e82a90063eafd4cfbaf12b48cb2
Scott Huntington <scott@pirateship.com>
2022-08-16 15:25:45 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'react-grid-system';
+import styled from 'styled-components';
+import { SIZES } from './constants';
+
+const StyledContainer = styled(Container)`
+  padding: ${SIZES.MD};
+  width: 720px;
+  border: 1px solid black;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`
+
+const Main = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  padding: ${SIZES.XL};
+`
+
+const Side = styled.div`
+  display: flex;
+  width: 200px;
+  flex-direction: column;
+  background-color: #FFF6F4;
+`
+
+const SideLink = styled.div`
+  padding: ${SIZES.MD};
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Side>
+        <SideLink>Prototypes</SideLink>
+        <SideLink>Ludography</SideLink>
+        <SideLink>About Us</SideLink>
+      </Side>
+      <Main>
+        <StyledContainer>
+          <Row>
+
+            <Col md={12}>
+              Hello
+            </Col>
+            <Col>
+              Hello
+            </Col>
+          </Row>
+        </StyledContainer>
+      </Main>
+    </Wrapper>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SIZES = {
+    XS: "2px",
+    SM: "4px",
+    MD: "8px",
+    LG: "16px",
+    XL: "32px"
+}
